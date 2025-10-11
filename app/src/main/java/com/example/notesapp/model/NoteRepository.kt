725a5cb9bdc6eff744fcbdab98e9951748c45a4d
arkellmer <arkellmer@hotmail.com>
2025-10-11 16:37:27 -0600

    Added repository.

--- app/src/main/java/com/example/notesapp/model/NoteRepository.kt
+++ app/src/main/java/com/example/notesapp/model/NoteRepository.kt
@@ -1,4 +1,35 @@
 package com.example.notesapp.model
 
-class Repository {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import java.util.Date
+
+class NoteRepository(
+    val scope: CoroutineScope,
+    private val dao: NoteDao
+) {
+    val notesAsc = dao.getAllNotesAsc()
+    val notesDesc = dao.getAllNotesDesc()
+
+    fun insertNote(text: String) {
+        scope.launch {
+            val now = Date()
+            dao.insertNote(NoteEntity(now, text))
+        }
+    }
+
+    fun updateNote(note: NoteEntity, text: String) {
+        scope.launch {
+            note.timestamp = Date()
+            note.text = text
+            dao.updateNote(note)
+        }
+    }
+
+    fun deleteNote(note: NoteEntity) {
+        scope.launch {
+            dao.deleteNote(note)
+        }
+    }
 }
