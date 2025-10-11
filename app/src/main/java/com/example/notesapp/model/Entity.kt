48ff111ea96460510e01bcca1aeb205ed234052e
arkellmer <arkellmer@hotmail.com>
2025-10-11 01:05:29 -0600

    Updated model to support dates.

--- app/src/main/java/com/example/notesapp/model/Entity.kt
+++ app/src/main/java/com/example/notesapp/model/Entity.kt
@@ -2,11 +2,24 @@ package com.example.notesapp.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
 import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
 
 @Entity(tableName = "notes")
 data class NoteEntity(
-    var date: Date,
+    var timestamp: Date,
     var text: String
 ) {
     @PrimaryKey(autoGenerate = true)
