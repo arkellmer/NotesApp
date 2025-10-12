7fd9679aa456ddd2770ecff2b6b86a506b4e21f4
arkellmer <arkellmer@hotmail.com>
2025-10-11 18:05:45 -0600

    Added comments.

--- app/src/main/java/com/example/notesapp/model/NoteRepository.kt
+++ app/src/main/java/com/example/notesapp/model/NoteRepository.kt
@@ -5,6 +5,13 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import java.util.Date
 
+/**
+ * A note repository which exposes the functionality of a note database.
+ *
+ * @property scope the scope of the application.
+ * @property dao the data access object of the database.
+ * @see NoteDatabase
+ */
 class NoteRepository(
     val scope: CoroutineScope,
     private val dao: NoteDao
@@ -12,6 +19,11 @@ class NoteRepository(
     val notesAsc = dao.getAllNotesAsc()
     val notesDesc = dao.getAllNotesDesc()
 
+    /**
+     * Inserts a note into the database.
+     *
+     * @property text the text of the note.
+     */
     fun insertNote(text: String) {
         scope.launch {
             val now = Date()
@@ -19,6 +31,12 @@ class NoteRepository(
         }
     }
 
+    /**
+     * Updates a note in the database.
+     *
+     * @property note the note to be updated.
+     * @property text the new text of the note.
+     */
     fun updateNote(note: NoteEntity, text: String) {
         scope.launch {
             note.timestamp = Date()
@@ -27,6 +45,11 @@ class NoteRepository(
         }
     }
 
+    /**
+     * Deletes a note in the database.
+     *
+     * @property note the note to be deleted.
+     */
     fun deleteNote(note: NoteEntity) {
         scope.launch {
             dao.deleteNote(note)
