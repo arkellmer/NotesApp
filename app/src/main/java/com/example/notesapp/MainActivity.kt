87a88738d5fed2ee23a30953cfd6b74c141f890f
arkellmer <arkellmer@hotmail.com>
2025-10-12 22:52:10 -0600

    Made the control bar transparent.

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.notesapp
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeContent
 import androidx.navigation.compose.rememberNavController
 import com.example.notesapp.navigation.NavHost
 import com.example.notesapp.ui.theme.NotesAppTheme
@@ -15,6 +19,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.setNavigationBarContrastEnforced(false)
+        }
         setContent {
             NotesAppTheme {
                 val vm: NoteViewModel by viewModels { NoteViewModelProvider.Factory }
